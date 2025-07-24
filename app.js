f1738975172f68ee4963c31221422214791945b9
tylisyt <93401762+tylisyt@users.noreply.github.com>
2025-07-24 12:37:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE = 'chatbot-frontend-production-42ef.up.railway.app';
+const API_BASE = 'https://chatbot-frontend-production-42ef.up.railway.app';
 
 async function analyzeWebsite() {
   const url = document.getElementById('urlInput').value;
